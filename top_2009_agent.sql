cbc63bcb45f10436abcc334c0826e7d4f59d0ebb
Marshall Offutt <40044635+marshalloffutt@users.noreply.github.com>
2019-04-19 12:32:55 -0500

    Rename 20top_2009_agent.sql to top_2009_agent.sql

--- top_2009_agent.sql
+++ top_2009_agent.sql
@@ -1,5 +1,5 @@
 --top_2009_agent.sql: Which sales agent 
---made the most in sales in 2009? HINT: MAX
+--made the most in sales in 2009? HINT:Â MAX
 
 select Top 1 max(i.total) as TotalSales, e.[FirstName] + ' ' + e.[LastName] SalesRep
 from Employee e
