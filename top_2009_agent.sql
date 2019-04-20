71e36f95c593db3b843f1b5e14dd1a2f0a746b1d
unknown <marshalloffutt@gmail.com>
2019-04-20 09:11:44 -0500

    using the max method

--- top_2009_agent.sql
+++ top_2009_agent.sql
@@ -1,10 +1,16 @@
 --top_2009_agent.sql: Which sales agent 
 --made the most in sales in 2009? HINT: MAX
 
-select Top 1 max(i.total) as TotalSales, e.[FirstName] + ' ' + e.[LastName] SalesRep
+drop table #sales
+select Top 1 sum(i.total) as TotalSales, e.[FirstName] + ' ' + e.[LastName] SalesRep
+into #sales
 from Employee e
-join Customer c on c.CustomerId = e.EmployeeId
+join Customer c on c.SupportRepId = e.EmployeeId
 join Invoice i on i.CustomerId = c.CustomerId
 where year (i.InvoiceDate) = '2009' and
 e.Title = 'Sales Support Agent'
 group by e.FirstName, e.LastName
+
+select max(TotalSales), SalesRep
+from #sales
+group by SalesRep
